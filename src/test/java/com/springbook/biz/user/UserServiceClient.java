5439253d495798c17d65f3c2836c794bb689d830
YooJungHyun <flywjdgus10@interpark.com>
2017-07-25 14:02:26 +0900

    2일차 4장까지

--- src/test/java/com/springbook/biz/user/UserServiceClient.java
+++ src/test/java/com/springbook/biz/user/UserServiceClient.java
@@ -8,9 +8,9 @@ public class UserServiceClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
-		
+
 		UserService userService = (UserService)container.getBean("userService");
-		
+
 		UserVO vo = new UserVO();
 		vo.setId("test");
 		vo.setPassword("test123");
